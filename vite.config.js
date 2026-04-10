699300622af6b82504e8bdecd29fc5aa94e258b3
Alaska45l <alaska45l@hotmail.com>
2026-04-10 13:30:44 -0300

    new vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  
   publicDir: '../public',
-  
   build: {
     outDir: '../dist',
-    emptyOutDir: true 
+    emptyOutDir: true
   }
 });
